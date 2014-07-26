a617c91679fb1e2fefb73bb351f2a727df9b6436
Vladimir Petrakovich <me@frostbit.tk>
2014-07-26 10:06:07 +0700

    implement list and tab navigation

--- app/src/main/java/com/noveogroup/task3/ListNavigationActivity.java
+++ app/src/main/java/com/noveogroup/task3/ListNavigationActivity.java
@@ -1,4 +1,35 @@
 package com.noveogroup.task3;
 
-public class ListNavigationActivity extends AbsMenuActivity {
+import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.widget.ArrayAdapter;
+import android.widget.Toast;
+
+public class ListNavigationActivity extends AbsMenuActivity
+        implements ActionBar.OnNavigationListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ActionBar ab = getSupportActionBar();
+        ab.setDisplayShowTitleEnabled(false);
+        ab.setNavigationMode(ActionBar.NAVIGATION_MODE_LIST);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                this,
+                android.R.layout.simple_spinner_item,
+                getResources().getStringArray(R.array.list_items)
+        );
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        ab.setListNavigationCallbacks(adapter, this);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(int pos, long id) {
+        Toast.makeText(
+                this,
+                getResources().getString(R.string.list_item_selected_format, pos),
+                Toast.LENGTH_SHORT).show();
+        return true;
+    }
+
 }
--- app/src/main/java/com/noveogroup/task3/TabNavigationActivity.java
+++ app/src/main/java/com/noveogroup/task3/TabNavigationActivity.java
@@ -1,17 +1,40 @@
 package com.noveogroup.task3;
 
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.v4.app.FragmentTabHost;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.ActionBar;
 import android.widget.TabHost;
+import android.widget.Toast;
 
-public class TabNavigationActivity extends AbsMenuActivity {
+public class TabNavigationActivity extends AbsMenuActivity implements ActionBar.TabListener {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         ActionBar ab = getSupportActionBar();
+        Resources res = getResources();
         ab.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
-
-        ab.addTab();
+        for (int i = 1; i <= 3; i++) {
+            ActionBar.Tab tab = ab.newTab();
+            tab.setText(res.getString(R.string.tab_title_format, i));
+            tab.setTag(i);
+            tab.setTabListener(this);
+            ab.addTab(tab);
+        }
     }
+
+    @Override
+    public void onTabSelected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+        Toast.makeText(
+                this,
+                getResources().getString(R.string.tab_selected_format, (Integer)tab.getTag()),
+                Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) { }
+
+    @Override
+    public void onTabReselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) { }
 }
