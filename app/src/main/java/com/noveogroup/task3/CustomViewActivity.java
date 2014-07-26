c31110062f9a6357794edab42d15b253243d3875
Vladimir Petrakovich <me@frostbit.tk>
2014-07-26 10:43:56 +0700

    add custom view and search view

--- app/src/main/java/com/noveogroup/task3/CustomViewActivity.java
+++ app/src/main/java/com/noveogroup/task3/CustomViewActivity.java
@@ -1,4 +1,18 @@
 package com.noveogroup.task3;
 
+import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.view.View;
+
 public class CustomViewActivity extends AbsMenuActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        View customView  = new View(this);
+        customView.setBackgroundColor(0xFFFF0000);
+        ActionBar ab = getSupportActionBar();
+        ab.setCustomView(customView);
+        ab.setDisplayShowTitleEnabled(false);
+        ab.setDisplayShowCustomEnabled(true);
+    }
 }
