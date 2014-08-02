36e599b43ec1d4241d20915a4a917df7c7ab4941
Arseny Skurko <ars.skurko@yandex.ru>
2014-08-03 00:04:16 +0700

    add simple action bar activity

--- app/src/main/java/com/noveogroup/task3/RootActivity.java
+++ app/src/main/java/com/noveogroup/task3/RootActivity.java
@@ -1,22 +1,76 @@
 package com.noveogroup.task3;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 
-public class RootActivity extends ActionBarActivity {
+public class RootActivity extends /*ActionBarActivity*/ Activity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_root);
+
+       Button simpleActionBar = (Button)findViewById(R.id.simple_action_bar);
+       Button simpleActionBarWithUpButton = (Button)findViewById(R.id.simple_action_bar_up_button);
+       Button splitActionBar = (Button)findViewById(R.id.split_action_bar);
+       Button actionBarWithNavigationTabs = (Button)findViewById(R.id.action_bar_with_navigation_tabs);
+       Button actionBarWithNavigationList = (Button)findViewById(R.id.action_bar_with_navigation_list);
+       Button actionBarWithCustomView = (Button)findViewById(R.id.action_bar_with_custom_view);
+       Button actionBarWithCustomTheme = (Button)findViewById(R.id.action_bar_with_custom_theme);
+       Button actionBarWithActionView = (Button)findViewById(R.id.action_bar_with_action_view);
+
+        simpleActionBar.setOnClickListener(this);
+        simpleActionBarWithUpButton.setOnClickListener(this);
+        splitActionBar.setOnClickListener(this);
+        actionBarWithNavigationTabs.setOnClickListener(this);
+        actionBarWithNavigationList.setOnClickListener(this);
+        actionBarWithCustomView.setOnClickListener(this);
+        actionBarWithCustomTheme.setOnClickListener(this);
+        actionBarWithActionView.setOnClickListener(this);
     }
 
     public void dummy(View view) {
         Toast.makeText(this, R.string.dragon, Toast.LENGTH_SHORT).show();
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.simple_action_bar:
+                startActivity(new Intent(this, SimpleActionBarActivity.class));
+                break;
+            case R.id.simple_action_bar_up_button:
+                //TODO
+                break;
+            case R.id.split_action_bar:
+                //TODO
+                break;
+            case R.id.action_bar_with_navigation_tabs:
+                //TODO
+                break;
+            case R.id.action_bar_with_navigation_list:
+                //TODO
+                break;
+            case R.id.action_bar_with_custom_view:
+                //TODO
+                break;
+            case R.id.action_bar_with_custom_theme:
+                //TODO
+                break;
+            case R.id.action_bar_with_action_view:
+                //TODO
+                break;
+            default:
+                break;
+        }
     }
 }
---
+++ app/src/main/java/com/noveogroup/task3/SimpleActionBarActivity.java
@@ -0,0 +1,50 @@
+package com.noveogroup.task3;
+
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.noveogroup.task3.R;
+
+public class SimpleActionBarActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_simple_action_bar);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle(R.string.app_name);
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.simple_action_bar, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_refresh:
+                Toast.makeText(this, "Refresh button", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.action_search:
+                Toast.makeText(this, "Search button", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.action_share:
+                Toast.makeText(this, "Share button", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.action_settings:
+                Toast.makeText(this, "Settings button", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
